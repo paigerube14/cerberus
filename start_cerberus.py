3bc8444a78936b297f9a16d1377414c6fca0b894
Paige Rubendall <prubenda@redhat.com>
2021-08-19 13:09:19 -0400

    Adding namespace check by default

--- start_cerberus.py
+++ start_cerberus.py
@@ -70,6 +70,7 @@ def main(cfg):
         watch_nodes = config["cerberus"].get("watch_nodes", False)
         watch_cluster_operators = config["cerberus"].get("watch_cluster_operators", False)
         watch_namespaces = config["cerberus"].get("watch_namespaces", [])
+        watch_terminating_namespaces = config["cerberus"].get("watch_terminating_namespaces", True)
         watch_url_routes = config["cerberus"].get("watch_url_routes", [])
         watch_master_schedulable = config["cerberus"].get("watch_master_schedulable", {})
         cerberus_publish_status = config["cerberus"].get("cerberus_publish_status", False)
@@ -229,6 +230,7 @@ def main(cfg):
                     (watch_nodes_status, failed_nodes),
                     (watch_cluster_operators_status, failed_operators),
                     (failed_routes),
+                    (terminating_namespaces),
                 ) = pool.map(
                     smap,
                     [
@@ -245,6 +247,9 @@ def main(cfg):
                             iter_track_time,
                         ),
                         functools.partial(kubecli.process_routes, watch_url_routes, iter_track_time),
+                        functools.partial(
+                            kubecli.monitor_namespaces_status, watch_terminating_namespaces, iteration, iter_track_time
+                        ),
                     ],
                 )
 
@@ -316,6 +321,9 @@ def main(cfg):
                             component = "-".join(component)
                         dbcli.insert(datetime.now(), time.time(), 1, "pod crash", failures, component)
                     logging.info("")
+
+                if terminating_namespaces:
+                    logging.info("Iteration %s: Terminating namespaces %s" % (iteration, str(terminating_namespaces)))
 
                 # Logging the failed checking of routes
                 watch_routes_status = True
